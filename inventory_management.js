d3aa4251ecf1177b4d17ada2e9e3d84625c52264
lina <ambowers@usf.edu>
2024-09-13 19:56:52 -0400

    create processSale function

--- inventory_management.js
+++ inventory_management.js
@@ -2,9 +2,9 @@
 const inventory = [
     { name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
     { name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
-    { name: 'Tablet', price: 400, quantity: 1, lowStockLevel: 2 }, // quantity is 1 to test the if else
-    { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
-    { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 4 }       
+    { name: 'Tablet', price: 400, quantity: 1, lowStockLevel: 2 }, // quantity is 1 to test the functions
+    { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 }, 
+    { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 4 } // quantity is 3 to test the functions 
 ];
 //Task 2 create a functiobn to display product details
     
@@ -30,6 +30,7 @@ function updateStock (product, unitsSold) {
 };
 
 // Task 4 create a function to check low stock products
+
 function checkLowStock () {
     inventory.forEach (product => {
         if (product.quantity < product.lowStockLevel){
@@ -43,10 +44,28 @@ checkLowStock ();
 function calculateInventoryValue () {
     return inventory.reduce ((totalValue, product) => {
         return totalValue + (product.price * product.quantity)},0);
-}
+};
 // make a constant to print the calculated total val
 const inventoryTotalValue = calculateInventoryValue();
 console.log(`The total inventory value is $${inventoryTotalValue}`);
+
+// Task 6 create a function to process a sale
+
+function processSale (product, unitsSold) {
+    // use find here so it iterates through array w arrow function
+    const item = inventory.find (item => item.name === productName);
+    //^use item bc product is already declared
+if (item){ 
+    if (unitsSold > product.quantity){
+        console.log (`Currently not enough availible in stock, try purchasing a smaller amount`)}
+    else { updateStock (product, unitsSold)};
+}// using the layred if else statements allows for an error message for trying to buy more than in stock 
+else {
+    console.log (`The product ${productName} is not in the inventory`)}; 
+    // above statement is for if the product/item is not found
+};
+
+
 
 
 
