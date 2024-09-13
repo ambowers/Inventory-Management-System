fe9eb63244d0a96f8a81bf66b7b9055150ab9591
lina <ambowers@usf.edu>
2024-09-13 16:43:08 -0400

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -1,23 +1,35 @@
-// Task 1
+// Task 1 create an inventory array of product objects
 const inventory = [
     { name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
     { name: 'Smartphone', price: 800, quantity: 5, lowStockLevel: 2 },
-    { name: 'Tablet', price: 400, quantity: 7, lowStockLevel: 1 },
+    { name: 'Tablet', price: 400, quantity: 0, lowStockLevel: 1 }, // quantity is zero to test the if else
     { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
     { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 }       
 ];
-//Task 2
-    // use arrow function because data is an array, object is product
-const displayProductDetails = (product) => {
-    let stockStatus ;
-    // use if else to check stock status, references the inventory array for quantity and low stock level
-    if (product.quantity <= product.lowStockLevel) {
-        stockStatus = "Low Stock";
+//Task 2 create a functiobn to display product details
+    
+function displayProductDetails (product) {
+
+    if (product.quantity <= product.lowStockLevel) { //use if else to display different message according to low or high stock
+        console.log(`Product: ${product.name}, Price: $${product.price}, Quantity: ${product.quantity}, Stock Status: ${product.name} is low in stock`);
     } else {
-        stockStatus = "In Stock";
+        console.log(`Product: ${product.name}, Price: $${product.price}, Quantity: ${product.quantity}, Stock Status: ${product.name} is in stock`);
     }
-console.log (`Product: ${product.name}`);
-console.log (`Price: $${product.price}`);
-console.log (`Inventory amount: ${product.quantity}`);
-console.log (`Stock status: ${stockStatus}`);
 };
+inventory.forEach(displayProductDetails);
+
+// Task 3 create a function to update stock product stock after sales
+
+function updateStock (product, unitsSold) {
+    product.quantity -= unitsSold;
+if (product.quantity = 0) {
+    console.log (`${product.name} is out of stock`);
+        // again using if else to display message according to updated stock level
+} else { (product.quantity <= product.lowStockLevel) 
+    console.log (`${product.name} is low in stock`)}
+};
+
+
+
+
+
